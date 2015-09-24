aff0aee18ee9f904c8c79ad3b98f68f950abb13f
barro32 <barro32@hotmail.com>
2015-09-25 00:18:37 +0100

    cubes position after rotation now updates correctly and highlighting current move added

--- Assets/scriptController.cs
+++ Assets/scriptController.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class scriptController : MonoBehaviour
-{
+public class scriptController : MonoBehaviour {
 
     //public GameObject[] groupXTags;
     //public GameObject[] groupYTags;
@@ -13,12 +12,18 @@ public class scriptController : MonoBehaviour
 
     Vector3[] rotationAxes = new Vector3[2];
 
+    RaycastHit clickHit;
     Vector2 clickStart;
     Vector2 clickEnd;
     Vector2 swipe;
     string swipeDirection;
+    bool mouseButtonDown = false;
 
     public scriptCube cubeScript;
+
+    int faceIdx;
+    public Material[] faceMaterials = new Material[6];
+    public Material[] highlightedFaceMaterials = new Material[6];
 
     // Use this for initialization
     void Start() {
@@ -28,16 +33,51 @@ public class scriptController : MonoBehaviour
     // Update is called once per frame
     void Update() {
         handleClick();
+        if (mouseButtonDown) {
+            // get the end of the click position
+            Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            // creat a vector between the click start and end and normalize
+            Vector2 swipeTemp = new Vector2(mousePos.x - clickStart.x, mousePos.y - clickStart.y);
+            swipeTemp.Normalize();
+
+            // find direction of swipe and rotate cubes
+            if ((swipeTemp.y > 0 || swipeTemp.y < 0) && swipeTemp.x > -0.5f && swipeTemp.x < 0.5f) {
+                resetHighlightedCubes(cubesHor);
+                highlightCubes(cubesVer);
+            }
+            if ((swipeTemp.x > 0 || swipeTemp.x < 0) && swipeTemp.y > -0.5f && swipeTemp.y < 0.5f) {
+                resetHighlightedCubes(cubesVer);
+                highlightCubes(cubesHor);
+            }
+            if (swipeTemp.x > -0.5f && swipeTemp.x < 0.5f && swipeTemp.y > -0.5f && swipeTemp.y < 0.5f) {
+                resetHighlightedCubes(cubesHor);
+                resetHighlightedCubes(cubesVer);
+            }
+
+        }
     }
 
     void handleClick() {
         if (Input.GetMouseButtonDown(0)) {
+            mouseButtonDown = true;
             RaycastHit hitInfo = new RaycastHit();
             bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
             if (hit) { // find which face of a cube has been selected, the group it belongs to and the direction the rotate
 
+                clickHit = hitInfo;
+
                 // highlight hit face
-                //Material colour = hitInfo.transform.GetComponent<Renderer>().material;
+                Transform thisFace = hitInfo.transform;
+                Transform parent = thisFace.parent;
+                for (int i = 0; i < 6; i++) {
+                    if (parent.GetChild(i).transform == thisFace) {
+                        faceIdx = i;
+                        parent.GetChild(i).transform.GetComponent<Renderer>().material = highlightedFaceMaterials[i];
+                        break;
+                    }
+                }
+                //hitInfo.transform.GetComponent<Renderer>().material;
                 //string name  = colour.name+"Highlight";
                 //colour = new Material.
 
@@ -75,6 +115,11 @@ public class scriptController : MonoBehaviour
             }
         }
         if (Input.GetMouseButtonUp(0)) {
+            mouseButtonDown = false;
+
+            // reset face colour
+            clickHit.transform.parent.GetChild(faceIdx).GetComponent<Renderer>().material = faceMaterials[faceIdx];
+
             // get the end of the click position
             clickEnd = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
@@ -99,6 +144,8 @@ public class scriptController : MonoBehaviour
                 swipeDirection = "left";
                 rotateGroup(cubesHor, rotationAxes[1], false);
             }
+            resetHighlightedCubes(cubesHor);
+            resetHighlightedCubes(cubesVer);
         }
     }
 
@@ -114,6 +161,22 @@ public class scriptController : MonoBehaviour
             // update the cubes with their new positions and add them to their new groups
             cubeScript = tag.transform.parent.gameObject.GetComponent<scriptCube>();
             cubeScript.updateCube();
+        }
+    }
+
+    void highlightCubes(GameObject[] tags) {
+        foreach (GameObject tag in tags) {
+            for (int i = 0; i < 6; i++) {
+                tag.transform.parent.GetChild(i).GetComponent<Renderer>().material = highlightedFaceMaterials[i];
+            }
+        }
+    }
+
+    void resetHighlightedCubes(GameObject[] tags) {
+        foreach (GameObject tag in tags) {
+            for (int i = 0; i < 6; i++) {
+                tag.transform.parent.GetChild(i).GetComponent<Renderer>().material = faceMaterials[i];
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -6,17 +6,12 @@ Interactive puzzle cube created with Unity 3D
 
 Click the face of a single cube and swipe up / down / left / right to rotate the cubes 90 degrees in the desired direction
 
-## Issues
-
-Cubes don't always update their new position correctly
-
 ## Todo
 
-* Fix issues
-* Rotate camera
-* Add highlight for selected face
-* Add indication of the rotataion about to be preformed
+* Camera Control
+* Better indication of rotation about to be preformed
 * Shuffle cubes
+* Animation
 
 ## Demo
 
